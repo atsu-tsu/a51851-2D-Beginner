cd0d2e664adeb7584084436ed4919c889c656f39
atsu-tsu <fdferreira93@gmail.com>
2023-03-31 12:18:59 +0100

    10.Timing and Framerate

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,7 +7,8 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
