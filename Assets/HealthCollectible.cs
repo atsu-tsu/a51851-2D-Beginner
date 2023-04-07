ca40b426aae901cfb7691af3f6126ca505d4b792
atsu-tsu <fdferreira93@gmail.com>
2023-04-07 17:27:38 +0100

    Check Whether Ruby Needs Health

--- Assets/HealthCollectible.cs
+++ Assets/HealthCollectible.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
-{
+
 void OnTriggerEnter2D(Collider2D other)
 {
-    Debug.Log("Object that entered the trigger : " + other);
+    RubyController controller = other.GetComponent<RubyController>();
+
+    if (controller != null)
+    {
+        if (controller.currentHealth < controller.maxHealth)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
 }
 
-}
