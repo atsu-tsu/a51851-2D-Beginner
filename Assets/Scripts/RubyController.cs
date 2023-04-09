2289c24aad1163a070b80bc403875722d121dbb7
atsu-tsu <fdferreira93@gmail.com>
2023-04-09 18:25:52 +0100

    11.Modifying the RubyController Script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-
     public float speed = 3.0f;
 
     public int maxHealth = 5;
@@ -20,14 +19,16 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -35,6 +36,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -46,10 +59,9 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
-
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
@@ -58,6 +70,7 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
@@ -68,5 +81,4 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
